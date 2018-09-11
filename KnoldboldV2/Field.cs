681ccd8ddcacfdc81f971cd587091c79393d151a
Frederik Spang <frederik@progras.dk>
2018-09-11 22:34:07 +0200

    Initial commit

---
+++ KnoldboldV2/Field.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnoldboldV2
+{
+    class Field
+    {
+        public List<Tuple<Match, Match>> Rounds = new List<Tuple<Match, Match>>();
+        public List<Field> AllFields; 
+        public string Name;
+
+        private string Space = "    ";
+        public Field(string name) {
+            Name = name;
+        }
+
+        public override string ToString() {
+            StringBuilder str = new StringBuilder();
+            str.AppendLine(Name);
+            str.AppendLine();
+            for (int i = 0; i < Rounds.Count; i++) {
+                var m1 = Rounds[i].Item1;
+                var m2 = Rounds[i].Item2;
+                str.Append($"Runde {i + 1}  -");
+                str.AppendLine(Space + $"{m1}      &&      {m2}");
+                str.AppendLine();
+                str.AppendLine(FormatNextField(m1.T1, i));
+                str.AppendLine(FormatNextField(m1.T2, i));
+                str.AppendLine(FormatNextField(m2.T1, i));
+                str.AppendLine(FormatNextField(m2.T2, i));
+
+                str.AppendLine();
+                str.AppendLine();
+            }
+
+            str.AppendLine();
+            return str.ToString();
+        }
+
+        private string FormatNextField(Team team, int round) {
+            if (team.Name == "DUMMY-MATCH") {
+                return "";
+            } else {
+                return Space + $"{team} skal til {NextField(team, round)} næste gang.";
+            }
+        }
+
+        private string NextField(Team t, int round) {
+            try {
+                foreach (var field in AllFields) {
+                    if (field.Rounds[round + 1].Item1.Contains(t) ||
+                        field.Rounds[round + 1].Item2.Contains(t)) {
+                        return field.Name;
+                    }
+                }
+            }
+            catch (ArgumentOutOfRangeException e) {
+                return "ingen steder, for der er ikke flere kampe";
+            }
+            
+            /* Shouldn't happen :D */
+            return "KUNNE IKKE FINDE BANE";
+        }
+    }
+}
---
+++ KnoldboldV2/GamePlanner.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnoldboldV2
+{
+    class GamePlanner
+    {
+        /*
+         * Deler holdene op i to rækker; øvre og nedre:
+         *      1  2  3  4
+         *      8  7  6  5
+         * 
+         * Modstående hold (1 og 8, 2 og 7, osv.) spiller
+         * mod hinanden. Efter hver kamp roterer rækkerne
+         * med uret - hold 1 bliver altid stående:
+         * 
+         * Efter første kamp:
+         *
+         *      1  8  2  3
+         *      7  6  5  4
+         */
+        private List<Team> upper = new List<Team>();
+        private List<Team> lower = new List<Team>();
+
+        public GamePlanner(List<Team> teams) {
+            if (teams.Count % 2 != 0) {
+                teams.Add(new Team("DUMMY-HOLD - tag en pause"));
+            }
+
+            for (int i = 0; i < teams.Count / 2; i++) {
+                upper.Add(teams[i]);
+            }
+
+            for (int i = teams.Count / 2; i < teams.Count; i++) {
+                lower.Add(teams[i]);
+            }
+        }
+
+        public Stack<Match> GenerateRound() {
+            Stack<Match> toReturn = new Stack<Match>();
+            for (int i = 0; i < upper.Count; i++) {
+                toReturn.Push(new Match(upper[i], lower[i]));
+            }
+            RotateTeams();
+            return toReturn;
+        }
+
+        private void RotateTeams() {
+            upper.Insert(1, lower.First());
+            lower.Add(upper.Last());
+            lower.Remove(lower.First());
+            upper.Remove(upper.Last());
+        }
+    }
+}
